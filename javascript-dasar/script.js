bfd6efe8b58f9c23ae73018023e0a92f75dce4db
mhbb8897 <mahbub.ahwani89@gmail.com>
2024-09-14 15:42:12 +0700

    First Commit

---
+++ javascript-dasar/script.js
@@ -0,0 +1,26 @@
+// const variableSatu = "Variable satu";
+// console.log(variableSatu);
+
+// const variableDua = 2;
+// console.log(variableDua);
+
+// const variableTiga = 3.14;
+// console.log(variableTiga);
+
+// const variableEmpat = false;
+// console.log(variableEmpat);
+
+let data = true;
+let data2 = false;
+
+
+function coba(data, data2) {
+  if (data == true) {
+    data = "Berhasil Login";
+  } else {
+    data = "Gagal Login!";
+  }
+  console.log(data)
+}
+
+coba(data, data2);
